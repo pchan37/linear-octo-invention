4a96b09bebd2c5f768d34e73f3acf4fa7634e50e
Patrick Chan <patrickchan1999@gmail.com>
2017-02-15 21:39:11 -0500

    Finished all octos and added test cases

--- base_python/draw.py
+++ base_python/draw.py
@@ -1,10 +1,33 @@
 from display import *
 
 def draw_line(screen, x0, y0, x1, y1, color):
-    # draw_line1(screen, x0, y0, x1, y1, color)
-    draw_line2(screen, x0, y0, x1, y1, color)
+    print x0, y0, x1, y1
+    dx = x1 - x0
+    dy = y1 - y0
 
-def draw_line1( screen, x0, y0, x1, y1, color ):
+    
+    
+    if dx >= 0 and dy >=0:
+        if dx >= dy:
+            return draw_line1(screen, x0, y0, x1, y1, color)
+        return draw_line2(screen, x0, y0, x1, y1, color)
+            
+    elif dx >= 0 and dy <= 0:
+        if dx >= -dy:
+            return draw_line8(screen, x0, y0, x1, y1, color)
+        return draw_line7(screen, x0, y0, x1, y1, color)
+
+    elif dx <= 0 and dy >=0:
+        if -dx >= dy:
+            return draw_line8(screen, x1, y1, x0, y0, color)
+        return draw_line7(screen, x1, y1, x0, y0, color)
+    else:
+        if -dx >= -dy:
+            return draw_line1(screen, x1, y1, x0, y0, color)
+        return draw_line2(screen, x1, y1, x0, y0, color)
+            
+        
+def draw_line1(screen, x0, y0, x1, y1, color):
     delta_x = x1 - x0
     delta_y = y1 - y0
 
@@ -53,11 +76,21 @@ def draw_line7(screen, x0, y0, x1, y1, color):
             d += 2 * A
         y0 -= 1
         d -= 2 * B
-    plot(screen, color, x0, y0)
-    
-def main():
-    screen = new_screen()
-    draw_line(screen, 0, 0, 20, 10, [255, 0, 255])
-    
-if __name__ == '__main__':
-    main()
+    plot(screen, color, x1, y1)
+
+def draw_line8(screen, x0, y0, x1, y1, color):
+    delta_x = x1 - x0
+    delta_y = y1 - y0
+
+    A = delta_y
+    B = -delta_x
+
+    d = -2 * A + B
+    while x0 < x1:
+        plot(screen, color, x0, y0)
+        if d < 0:
+            y0 -= 1
+            d -= 2 * B
+        x0 += 1
+        d += 2 * A
+    plot(screen, color, x1, y1)
--- base_python/main.py
+++ base_python/main.py
@@ -1,12 +1,21 @@
+from __future__ import division
 from display import *
 from draw import *
 
-screen = new_screen()
-color = [ 0, 255, 0 ]
+from math import floor, pi, cos, sin
 
-draw_line1(screen, 0, 0, 20, 10, color)
-draw_line2(screen, 0, 0, 10, 20, color)
-draw_line7(screen, 10, 20, 0, 0, color)
+screen = new_screen()
+color = [ 120, 255, 120 ]
+
+interval = pi / 16
+def dy(theta):
+    return sin(interval * theta)
+
+def dx(theta):
+    return cos(interval * theta)
+
+for i in xrange(32):
+    draw_line(screen, 250, 250, 250 + int(250 * dx(i)), 250 + int(250 * dy(i)), color)
 
 display(screen)
 save_extension(screen, 'img.png')
